505d8d5fec855c64fe63c4d5c02d54bb22ebf46f
Krish01agrawal <krish.agrawal@plutomoney.in>
2025-11-15 18:05:27 +0530

    GlobalValidationPipes: ClassValidator, Class Transformer

--- NestJS/my-nest-app/src/customer/dto/create-customer.dto.ts
+++ NestJS/my-nest-app/src/customer/dto/create-customer.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString, IsNumber, IsInt } from 'class-validator';
+
 export class CreateCustomerDto {
+    @IsNotEmpty()
+    @IsString()
     name: string;
+
+    @IsNotEmpty()
+    @IsInt()
     age: number;
 }
