c1694ca3292d1e2b7fedc50f72314370027c5c98
Krish01agrawal <krish.agrawal@plutomoney.in>
2025-11-20 01:49:39 +0530

    Get and GetById implemented MongoDb Operations CRUD

--- NestJS/my-nest-app/src/student/student.controller.ts
+++ NestJS/my-nest-app/src/student/student.controller.ts
@@ -13,32 +13,42 @@ export class StudentController {
     }
 
     @Get()
-    getAllStudents(){
+    async getAllStudents(){
         return this.studentService.getAllStudents();
-    } 
+    }
 
     @Get(':id')
-    getStudentById(@Param('id') id: string){
-        return this.studentService.getStudentById(Number(id));
+    async getStudentById(@Param('id') id: string){
+        return this.studentService.getStudentById(id);
     }
+
+    // @Get()
+    // getAllStudents(){
+    //     return this.studentService.getAllStudents();
+    // } 
+
+    // @Get(':id')
+    // getStudentById(@Param('id') id: string){
+    //     return this.studentService.getStudentById(Number(id));
+    // }
 
     // @Post()
     // createStudent(@Body() data: {name: string, age: number}){
     //     return this.studentService.createStudent(data);
     // }
 
-    @Put(':id')
-    updateStudent(@Param('id') id: string, @Body() data: {name: string, age: number}){
-        return this.studentService.updateStudent(Number(id), data);
-    }
+    // @Put(':id')
+    // updateStudent(@Param('id') id: string, @Body() data: {name: string, age: number}){
+    //     return this.studentService.updateStudent(Number(id), data);
+    // }
 
-    @Patch(':id')
-    patchStudent(@Param('id') id: string, @Body() data: Partial<{name: string, age: number}>){
-        return this.studentService.patchStudent(Number(id), data);
-    }
+    // @Patch(':id')
+    // patchStudent(@Param('id') id: string, @Body() data: Partial<{name: string, age: number}>){
+    //     return this.studentService.patchStudent(Number(id), data);
+    // }
 
-    @Delete(':id')
-    deleteStudent(@Param('id') id: string){
-        return this.studentService.deleteStudent(Number(id));
-    }
+    // @Delete(':id')
+    // deleteStudent(@Param('id') id: string){
+    //     return this.studentService.deleteStudent(Number(id));
+    // }
 }
--- NestJS/my-nest-app/src/student/student.service.ts
+++ NestJS/my-nest-app/src/student/student.service.ts
@@ -15,81 +15,93 @@ export class StudentService {
         return createdStudent.save();
     }
 
-    private students = [
-        {
-            id: 1,
-            name: 'Student 1',
-            age: 20,
-        },
-        {
-            id: 2,
-            name: 'Student 2',
-            age: 21,
-        },
-        {
-            id: 3,
-            name: 'Student 3',
-            age: 22,
-        },
-    ];
+    async getAllStudents(): Promise<Student[]>{
+        return this.studentModel.find().exec();
+    }
+
+    async getStudentById(id: string): Promise<Student | null>{
+        const student = await this.studentModel.findById(id).exec() as unknown as Student;
+        if(!student){
+            throw new HttpException('Student not found', HttpStatus.NOT_FOUND);
+        }
+        return student;
+    }
+
+    // private students = [
+    //     {
+    //         id: 1,
+    //         name: 'Student 1',
+    //         age: 20,
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Student 2',
+    //         age: 21,
+    //     },
+    //     {
+    //         id: 3,
+    //         name: 'Student 3',
+    //         age: 22,
+    //     },
+    // ];
 
     //Get
-    getAllStudents(){
-        return this.students;
-    }
+    // getAllStudents(){
+    //     return this.students;
+    // }
 
-    getStudentById(id: number){
-        const student = this.students.find((student) => student.id === id);
-        if(!student){
-            throw new HttpException('Student not found', HttpStatus.NOT_FOUND);
-        }
-        return student;
-    }
+    // getStudentById(id: number){
+    //     const student = this.students.find((student) => student.id === id);
+    //     if(!student){
+    //         throw new HttpException('Student not found', HttpStatus.NOT_FOUND);
+    //     }
+    //     return student;
+    // }
 
-    //Post
-    createStudentt(data: {name: string, age: number}){
-        const student = {
-            id: this.students.length + 1,
-            ...data,
-        };
-        this.students.push(student);
-        return student;
-    }
+    // //Post
+    // createStudent(data: {name: string, age: number}){
+    //     const student = {
+    //         id: this.students.length + 1,
+    //         ...data,
+    //     };
+    //     this.students.push(student);
+    //     return student;
+    // }
 
-    //Put
-    updateStudent(id: number, data: {name: string, age: number}){
-        const index = this.students.findIndex((student) => student.id === id);
-        if(index === -1){
-            throw new HttpException('Student not found', HttpStatus.NOT_FOUND);
-        }
-        this.students[index] = {
-            id,
-            ...data,
-        };
-        return this.students[index];
-    }
+    // //Put
+    // updateStudent(id: number, data: {name: string, age: number}){
+    //     const index = this.students.findIndex((student) => student.id === id);
+    //     if(index === -1){
+    //         throw new HttpException('Student not found', HttpStatus.NOT_FOUND);
+    //     }
+    //     this.students[index] = {
+    //         id,
+    //         ...data,
+    //     };
+    //     return this.students[index];
+    // }
 
-    //Patch
-    patchStudent(id: number, data: Partial<{name: string, age: number}>){
-        const student = this.getStudentById(id);
-        if(!student){
-            throw new HttpException('Student not found', HttpStatus.NOT_FOUND);
-        }
-        Object.assign(student, data);   //only modify the fields that are provided
-        return student;
-    }
+    // //Patch
+    // patchStudent(id: number, data: Partial<{name: string, age: number}>){
+    //     const student = this.getStudentById(id);
+    //     if(!student){
+    //         throw new HttpException('Student not found', HttpStatus.NOT_FOUND);
+    //     }
+    //     Object.assign(student, data);   //only modify the fields that are provided
+    //     return student;
+    // }
 
-    //Delete
-    deleteStudent(id: number){
-        const index = this.students.findIndex((student) => student.id === id);
-        if(index === -1){
-            throw new HttpException('Student not found', HttpStatus.NOT_FOUND);
-        }
-        // this.students = this.students.filter((student) => student.id !== id);
-        const deletedStudent = this.students.splice(index, 1);  //remove the student from the array
-        return {
-            message: 'Student deleted successfully',
-            student: deletedStudent,
-        };
-    }
+    // //Delete
+    // deleteStudent(id: number){
+    //     const index = this.students.findIndex((student) => student.id === id);
+    //     if(index === -1){
+    //         throw new HttpException('Student not found', HttpStatus.NOT_FOUND);
+    //     }
+    //     // this.students = this.students.filter((student) => student.id !== id);
+    //     const deletedStudent = this.students.splice(index, 1);  //remove the student from the array
+    //     return {
+    //         message: 'Student deleted successfully',
+    //         student: deletedStudent,
+    //     };
+    // }
 }
